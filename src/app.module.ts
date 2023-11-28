afae17c91f9683373ef6ec40f5263b40cbf1c808
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-28 21:45:00 +0800

    feat: add telegraf module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
 
 import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [CoreModule],
+  imports: [
+    CoreModule,
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        token: configService.get('TELEGRAM_BOT_TOKEN') as string,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
