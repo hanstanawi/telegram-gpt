b8d542a0f06e6c226defe4c092394faddd665d4b
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-29 23:46:06 +0800

    chore: move telegraf types to common

--- src/common/types/telegram.types.ts
+++ src/common/types/telegram.types.ts
@@ -2,3 +2,4 @@ import type { Message, User } from '@telegraf/types';
 
 export type TelegramMessage = Message.ServiceMessage;
 export type TelegramUser = User;
+export type TelegramTextMessage = Message.TextMessage;
