36c5a2f7972373df31c49ba6fdb2010a9229cf98
Hans Tanawi <hans.tanawi@outlook.com>
2023-11-29 13:50:37 +0800

    feat: setup openai services

---
+++ src/modules/openai/services/index.ts
@@ -0,0 +1,3 @@
+export * from './openai.service';
+export * from './openai-audio.service';
+export * from './openai-chat.service';
--- src/modules/openai/services/openai-chat.service.spec.ts
+++ src/modules/openai/services/openai-chat.service.spec.ts
@@ -1,16 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { OpenAiService } from './openai.service';
+import { OpenAiChatService } from './openai-chat.service';
 
-describe('OpenaiService', () => {
-  let service: OpenAiService;
+describe('OpenAiChatService', () => {
+  let service: OpenAiChatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OpenAiService],
+      providers: [OpenAiChatService],
     }).compile();
 
-    service = module.get<OpenAiService>(OpenAiService);
+    service = module.get<OpenAiChatService>(OpenAiChatService);
   });
 
   it('should be defined', () => {
